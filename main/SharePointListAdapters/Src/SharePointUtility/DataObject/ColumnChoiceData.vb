851e2cfb8e231e547f96634996d4874ebaa3dc3b
MattCarroll_cp <SND\MattCarroll_cp>
2008-08-16 00:57:06 +0000

    Initial check-in of SharePointListAdapters (code written by Kevin Idzi)

---
+++ main/SharePointListAdapters/Src/SharePointUtility/DataObject/ColumnChoiceData.vb
@@ -0,0 +1,26 @@
+﻿Namespace DataObject
+    ''' <summary>
+    ''' Data Object for SharePoint Field Choices
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class ColumnChoiceData
+
+        Private _name As String
+
+        ''' <summary>
+        ''' Name of a choice among choices specified by the user in SharePoint
+        ''' </summary>
+        ''' <value></value>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Property Name() As String
+            Get
+                Return _name
+            End Get
+            Friend Set(ByVal value As String)
+                _name = value
+            End Set
+        End Property
+
+    End Class
+End Namespace
